f832b92b1ab091c23c9bda12be39aef002c93be0
Frank <xuzy213@gmail.com>
2018-05-24 22:12:54 +1200

    add DragSource into Backlog component

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,13 +1,38 @@
 import React from 'react'
+import {DragDropContext} from 'react-dnd'
+import HTML5Backend from 'react-dnd-html5-backend'
+import Backlog from './Backlog'
 
-import Words from './Words'
-import AddWord from './AddWord'
+class App extends React.Component {
+  render () {
+    return (
+      <div className='app-container'>
+        <div className="tile is-ancestor">
+          <div className="tile is-vertical is-parent">
+            <div className="tile is-child box">
+              <p className="title">Backlog</p>
+              <Backlog />
+            </div>
+          </div>
+          <div className="tile is-parent">
+            <div className="tile is-child box">
+              <p className="title">In Progress</p>
+            </div>
+          </div>
+          <div className="tile is-parent">
+            <div className="tile is-child box">
+              <p className="title">In Review</p>
+            </div>
+          </div>
+          <div className="tile is-parent">
+            <div className="tile is-child box">
+              <p className="title">Done</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
 
-const App = () => (
-  <div className='app-container'>
-    <Words />
-    <AddWord />
-  </div>
-)
-
-export default App
+export default DragDropContext(HTML5Backend)(App)
---
+++ client/components/Backlog.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+
+import {DragSource} from 'react-dnd'
+
+const backlogSource = {
+  beginDrag (props) {
+    return {}
+  }
+}
+
+function collect (connect, monitor) {
+  return {
+    connectDragSource: connect.dragSource(),
+    isDragging: monitor.isDragging()
+  }
+}
+
+class Backlog extends React.Component {
+  render () {
+    const {connectDragSource, isDragging} = this.props
+    return connectDragSource(
+      <div style={{
+        opacity: isDragging ? 0.5 : 1,
+        fontSize: 25,
+        fontWeight: 'bold',
+        cursor: 'move'
+      }}
+      className="card">
+        <header className="card-header">
+          <p className="card-header-title">
+            Component
+          </p>
+        </header>
+        <div className="card-content">
+          <div className="content">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.
+            <br />
+            <time dateTime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+          </div>
+        </div>
+        <footer className="card-footer">
+          <a href="#" className="card-footer-item">Save</a>
+          <a href="#" className="card-footer-item">Edit</a>
+          <a href="#" className="card-footer-item">Delete</a>
+        </footer>
+      </div>
+    )
+  }
+}
+
+export default DragSource('backlog', backlogSource, collect)(Backlog)
